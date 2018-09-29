4f7ad60dcfa00c6eb4c1852ac88e0dc8521c5a53
barankia <laima.zickute@gmail.com>
2018-09-29 16:34:57 +0200

    "second try"

---
+++ src/main/java/com/noname/workshop/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.noname.workshop.repository.search;
+
+import com.noname.workshop.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
---
+++ src/main/java/com/noname/workshop/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package com.noname.workshop.service.mapper;
