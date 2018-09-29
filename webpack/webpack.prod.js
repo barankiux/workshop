adbc275c2b8ded1003f4c357fa8e774fb49da9d0
barankia <laima.zickute@gmail.com>
2018-09-29 18:20:20 +0200

    config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -87,6 +87,9 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             })
         ]
     },
+    performance: {
+        hints: process.env.NODE_ENV === 'production' ? "warning" : false
+      },
     plugins: [
         extractCSS,
         new MomentLocalesPlugin({
